a618340a56dc634f8eb0e9386bfb12f9d198b25b
JuggernautTej <tiwatej@gmail.com>
2023-10-18 18:01:24 -0700

    Edited the read.c program and the AUTHORS program

--- read.c
+++ read.c
@@ -7,21 +7,20 @@
  */
 char *read_cmd(void)
 {
-	char *cmd = NULL;
-	size_t size = 0;
-	ssize_t character_read;
-
-	_printstrg("ENTER INPUT: ");
-
-	character_read = getline(&cmd, &size, stdin);
-
-	if (character_read == -1){
-		_printstrg("Error while reading input.\n");
-		free (cmd);
-		return NULL;
-	}
-	if (cmd[character_read - 1] == '\n'){
-		cmd[character_read -1] = '\0';
-	}
-	return cmd;
+char *cmd = NULL;
+size_t size = 0;
+ssize_t character_read;
+_printstrg("ENTER INPUT: ");
+character_read = getline(&cmd, &size, stdin);
+if (character_read == -1)
+{
+_printstrg("Error while reading input.\n");
+free (cmd);
+return (NULL);
+}
+if (cmd[character_read - 1] == '\n')
+{
+cmd[character_read -1] = '\0';
+}
+return (cmd);
 }
